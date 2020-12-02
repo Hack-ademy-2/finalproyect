8941265a131fcef62585a2f5e067ed94d0bf06ae
NAHU <nahuelbarbieri.93@gmail.com>
2020-12-02 17:10:46 +0100

    migration-authmiddleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +14,5 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', [PageController::class,'index'])->name('welcome');
+Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/announcement/new', [HomeController::class,'newAnnouncement'])->name('announcement.new');
