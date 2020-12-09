f23161b747c632c565a583d1eded7194b69d0980
NAHU <nahuelbarbieri.93@gmail.com>
2020-12-09 12:54:37 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,8 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>{{ __('ui.welcome') }}</h1>
+            <div class="col-12 text-center">
+                <h1 class="mb-0 mt-4">{{ __('ui.welcome') }}</h1>
             </div>
 
             @if(session('access.denied.revisor.only'))
