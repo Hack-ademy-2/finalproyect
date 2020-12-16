855f8c7e464f99cd1ec87bde83ddd51e7d74e0bf
NAHU <nahuelbarbieri.93@gmail.com>
2020-12-16 11:01:01 +0100

    changes

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -9,23 +9,23 @@
         <div class='col-12'>
             <div class="card">
                 <div class="card-header">
-                {{ __('ui.anuncio')}} #{{$announcement->id}}
+                    ID {{ __('ui.anuncio')}} #{{$announcement->id}}
                 </div>
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <div class="row">
                         <div class="col-md-3">
-                            <h3>{{ __('ui.usuario')}}</h3>
+                            <h5>{{ __('ui.usuario')}}</h5>
                         </div>
                         <div class="col-md-9">
-                            #{{$announcement->user->id}}
-                            {{$announcement->user->name}}
+                            ID #{{$announcement->user->id}} <br>
+                            {{$announcement->user->name}} <br>
                             {{$announcement->user->email}}
                         </div>
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-md-3">
-                            <h3>{{ __('ui.titulo')}}</h3>
+                            <h5>{{ __('ui.titulo')}}</h5>
                         </div>
                         <div class="col-md-9">
                             {{$announcement->title}}
@@ -34,7 +34,7 @@
                     <hr>
                     <div class="row">
                         <div class="col-md-3">
-                            <h3>{{ __('ui.descripcion')}}</h3>
+                            <h5>{{ __('ui.descripcion')}}</h5>
                         </div>
                         <div class="col-md-9">
                             {{$announcement->body}}
@@ -42,29 +42,33 @@
                     </div>
                     <hr>
                     <div class="row">
-                        <div class="col-md-3">
-                            <h3>{{ __('ui.imagenes')}}</h3>
+                        <div class="col-md-3 mb-3">
+                            <h5>{{ __('ui.imagenes')}}</h5>
                         </div>
-                        <div class="col-md-10">
+                        <div class="row">
+                            <div class="col-md-6 d-flex justify-content-center">
+                                @include ('announcement._announcement_carousel')
+                            </div>
                             @foreach ($announcement->images as $image)
-                            <div class="row my-5">
-                                <div class="col-md-8">
-                                    <p>Adult: {{$image->adult}} - Spoof: {{$image->spoof}} </p>
-                                    <p>Medical: {{$image->medical}} - Violence: {{$image->violence}}</p>
-                                    <p>Racy: {{$image->racy}}</p>
-                                    <p>Labels:</p>
-                                    <ul>
-                                        @if($image->labels)
-                                            @foreach ($image->labels as $label)
-                                            <li>{{$label}}</li>
-                                            @endforeach
-                                        @endif
-                                    </ul>
-                                    <!-- ID: {{$image->id}} <br> URL:
+                            <div class="col-md-6 d-flex flex-column justify-content-center">
+                                <p>Adult: {{$image->adult}} - Spoof: {{$image->spoof}} </p>
+                                <p>Medical: {{$image->medical}} - Violence: {{$image->violence}}</p>
+                                <p>Racy: {{$image->racy}}</p>
+                                <!-- ID: {{$image->id}} <br> URL:
                                     {{Storage::url($image->file)}} <br> -->
+                            </div>
+                            <div class="col-12">
+                            <div class="dropdown float-right">
+                                <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Labels
+                                </button>
+                                @if($image->labels)
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                @foreach ($image->labels as $label)
+                                    <a class="dropdown-item" href="#">{{$label}}</a>
+                                @endforeach
                                 </div>
-                                <div class="col-md-4">
-                                    <img src="{{$image->getUrl(300, 150)}}" class="rounded" alt="">
+                                @endif
                                 </div>
                             </div>
                             @endforeach
@@ -75,13 +79,13 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-6 text-right">
             <form action="{{route('revisor.announcement.reject',['id'=>$announcement->id])}}" method="POST">
                 @csrf
                 <button type="submit" class="btn btn-danger">{{ __('ui.rechazar')}}</button>
             </form>
         </div>
-        <div class="col-md-6 text-right">
+        <div class="col-6 text-left">
             <form action="{{route('revisor.announcement.accept',['id'=>$announcement->id])}}" method="POST">
                 @csrf
                 <button type="submit" class="btn btn-success mb-5">{{ __('ui.aceptar')}}</button>
