0a8809c8339e2a86f44109690af8551520b53f24
Francisco Javier Niño Miralles <azza@MacBook-Pro-de-Francisco-2.local>
2020-12-16 11:38:53 +0100

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 <body>
     @include('layouts._navbar')
 
-    <div class="container">
+    <div class="container main-content">
         <div class="row">
             @if(session('access.denied.revisor.only'))
             <div class="col-12 alert alert-danger mt-3 text-center">{{session('access.denied.revisor.only')}}</div>
